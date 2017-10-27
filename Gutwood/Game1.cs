8b7eb6de1deb0e52ee0b5832b2a0de00ffedf51d
Cole Knutson <cole.knutson1@gmail.com>
2017-10-26 21:59:11 -0500

    Safe Keepin

--- Gutwood/Game1.cs
+++ Gutwood/Game1.cs
@@ -20,7 +20,6 @@ namespace Gutwood
         private SpriteFont font;
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        int CurrentDirection;
 
         Player player;
         BaseObject background;
@@ -59,19 +58,20 @@ namespace Gutwood
             mouse = new BaseObject();
             tree = new BaseObject();
             tree2 = new BaseObject();
-            background.SpriteScale = 4f; //HACK FIGURE OUT ASAP!!!!
             TouchPanel.EnabledGestures = GestureType.FreeDrag;
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
+            allCollisionRectangles.Add(tree.CollisionRectangles);
+            allCollisionRectangles.Add(tree2.CollisionRectangles);
             bulletTexture = Content.Load<Texture2D>("Bullet");
             spriteBatch = new SpriteBatch(GraphicsDevice);
             Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y +
                 GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
             player.Initialize(Content.Load<Texture2D>("Mario"), playerPosition);
-            background.Initialize(Content.Load<Texture2D>("grass-1"), new Vector2(0, 0));
+            background.Initialize(Content.Load<Texture2D>("Background"), new Vector2(0, 0));
             mouse.Initialize(Content.Load<Texture2D>("crosshair"), new Vector2(0, 0));
             tree.Initialize(Content.Load<Texture2D>("Tree"), new Vector2(250, 250), "Tree", isCollidable: true);
             tree2.Initialize(Content.Load<Texture2D>("Tree"), new Vector2(500, 100), "Tree2", isCollidable: true);
@@ -79,88 +79,20 @@ namespace Gutwood
 
         protected override void UnloadContent()
         {
-
+            bulletTexture.Dispose();
+            spriteBatch.Dispose();            
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            previousGamePadState = currentGamePadState;
-            previousKeyboardState = currentKeyboardState;
-            previousMouseState = currentMouseState;
-
-            currentKeyboardState = Keyboard.GetState();
-            currentGamePadState = GamePad.GetState(PlayerIndex.One);
-            currentMouseState = Mouse.GetState();
-
-            mouse.Position.X = currentMouseState.X - mouse.BaseObjectTexture.Width / 2; mouse.Position.Y = currentMouseState.Y - mouse.BaseObjectTexture.Height / 2;
-
-
-            //HACK FOR TESTING COLLISION ALGORITHM!
-            //
-
-            allCollisionRectangles.Add(tree.CollisionRectangles);
-            allCollisionRectangles.Add(tree2.CollisionRectangles);
             player.UpdateCollision(allCollisionRectangles);
-            
-            UpdatePlayer();
-
-            base.Update(gameTime);
-        }      
-
-        public void UpdatePlayer()
-        {
-
-            player.IsRunning = currentKeyboardState.IsKeyDown(Keys.LeftShift) ? false : true;
-            if (currentMouseState.RightButton == ButtonState.Released) {
-                if (!player.CollidingLeft && (currentKeyboardState.IsKeyDown(Keys.Left) || currentKeyboardState.IsKeyDown(Keys.A) ||
-                     currentGamePadState.DPad.Left == ButtonState.Pressed || (currentGamePadState.ThumbSticks.Left.X >= -1 && currentGamePadState.ThumbSticks.Left.X < -.3)))
-                {
-                    player.Position.X -= player.Speed;
-                }
-
-                if (!player.CollidingRight && (currentKeyboardState.IsKeyDown(Keys.Right) || currentKeyboardState.IsKeyDown(Keys.D) ||
-                     currentGamePadState.DPad.Right == ButtonState.Pressed || (currentGamePadState.ThumbSticks.Left.X <= 1 && currentGamePadState.ThumbSticks.Left.X > .3)))
-                {
-                    player.Position.X += player.Speed;
-                }
-
-                if (!player.CollidingTop && (currentKeyboardState.IsKeyDown(Keys.Up) || currentKeyboardState.IsKeyDown(Keys.W) ||
-                    currentGamePadState.DPad.Up == ButtonState.Pressed || (currentGamePadState.ThumbSticks.Left.Y <= 1 && currentGamePadState.ThumbSticks.Left.Y > .3)))
-                {
-                    player.Position.Y -= player.Speed;
-                }
-
-                if (!player.CollidingBottom && (currentKeyboardState.IsKeyDown(Keys.Down) || currentKeyboardState.IsKeyDown(Keys.S) ||
-                     currentGamePadState.DPad.Down == ButtonState.Pressed || (currentGamePadState.ThumbSticks.Left.Y >= -1 && currentGamePadState.ThumbSticks.Left.Y < -.3)))
-                {
-                    player.Position.Y += player.Speed;
-                }
-            }
-
-            if(player.IsRunning)
-            {
-                player.Speed = 5;
-            }
-            else
-            {
-                player.Speed = 3;
-            }
-
-            //Make sure we don't go out of bounds
-            player.Position.X = MathHelper.Clamp(player.Position.X, 0, GraphicsDevice.Viewport.Width - player.Width);
-            player.Position.Y = MathHelper.Clamp(player.Position.Y, 0, GraphicsDevice.Viewport.Height - player.Height);
-
-
-            if (previousMouseState.LeftButton == ButtonState.Released && currentMouseState.LeftButton == ButtonState.Pressed && currentMouseState.RightButton == ButtonState.Pressed)
-            {
-                Vector2 mousePosition = new Vector2(currentMouseState.X, currentMouseState.Y);
-                Vector2 middleOfPlayer = new Vector2(player.Position.X+player.Width/2, player.Position.Y+player.Height/2);
-                bullets.Add(new Bullet(bulletTexture, middleOfPlayer, mousePosition));
-            }
+            HandleInput();
+            HandleMovement();
+            HandleScreenBounds();
+            HandleRunning();
+            HandleGunShot();
             UpdateBullets();
+            base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
@@ -247,6 +179,105 @@ namespace Gutwood
             {
                 player.movingLeft = true;
             }
+        }
+
+        private void HandleGunShot()
+        {
+            if (previousMouseState.LeftButton == ButtonState.Released && currentMouseState.LeftButton == ButtonState.Pressed && currentMouseState.RightButton == ButtonState.Pressed)
+            {
+                Vector2 mousePosition = new Vector2(currentMouseState.X, currentMouseState.Y);
+                Vector2 middleOfPlayer = new Vector2(player.Position.X + player.Width / 2, player.Position.Y + player.Height / 2);
+                bullets.Add(new Bullet(bulletTexture, middleOfPlayer, mousePosition));
+            }
+        }
+
+        private void HandleRunning()
+        {
+            //Increase speed if running
+            if (player.IsRunning)
+            {
+                player.Speed = 5;
+            }
+            //Reduce speed if running
+            else
+            {
+                player.Speed = 3;
+            }
+        }
+
+        private void HandleScreenBounds()
+        {
+            //Make sure we don't go out of bounds
+            player.Position.X = MathHelper.Clamp(player.Position.X, 0, GraphicsDevice.Viewport.Width - player.Width);
+            player.Position.Y = MathHelper.Clamp(player.Position.Y, 0, GraphicsDevice.Viewport.Height - player.Height);
+        }
+
+        private void HandleMovement()
+        {
+            //Check if they're attempting to walk
+            player.IsRunning = currentKeyboardState.IsKeyDown(Keys.LeftShift) ? false : true;
+
+            //Handle movement
+            if (currentMouseState.RightButton == ButtonState.Released)
+            {
+                if (!player.CollidingLeft && (currentKeyboardState.IsKeyDown(Keys.Left) || currentKeyboardState.IsKeyDown(Keys.A) ||
+                     currentGamePadState.DPad.Left == ButtonState.Pressed || (currentGamePadState.ThumbSticks.Left.X >= -1 && currentGamePadState.ThumbSticks.Left.X < -.3)))
+                {
+                    player.Position.X -= player.Speed;
+                }
+
+                if (!player.CollidingRight && (currentKeyboardState.IsKeyDown(Keys.Right) || currentKeyboardState.IsKeyDown(Keys.D) ||
+                     currentGamePadState.DPad.Right == ButtonState.Pressed || (currentGamePadState.ThumbSticks.Left.X <= 1 && currentGamePadState.ThumbSticks.Left.X > .3)))
+                {
+                    player.Position.X += player.Speed;
+                }
+
+                if (!player.CollidingTop && (currentKeyboardState.IsKeyDown(Keys.Up) || currentKeyboardState.IsKeyDown(Keys.W) ||
+                    currentGamePadState.DPad.Up == ButtonState.Pressed || (currentGamePadState.ThumbSticks.Left.Y <= 1 && currentGamePadState.ThumbSticks.Left.Y > .3)))
+                {
+                    player.Position.Y -= player.Speed;
+                }
+
+                if (!player.CollidingBottom && (currentKeyboardState.IsKeyDown(Keys.Down) || currentKeyboardState.IsKeyDown(Keys.S) ||
+                     currentGamePadState.DPad.Down == ButtonState.Pressed || (currentGamePadState.ThumbSticks.Left.Y >= -1 && currentGamePadState.ThumbSticks.Left.Y < -.3)))
+                {
+                    player.Position.Y += player.Speed;
+                }
+            }
+        }
+
+        private void HandleInput()
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (currentKeyboardState.IsKeyDown(Keys.F11) && !previousKeyboardState.IsKeyDown(Keys.F11))
+                graphics.ToggleFullScreen();
+
+            previousGamePadState = currentGamePadState;
+            previousKeyboardState = currentKeyboardState;
+            previousMouseState = currentMouseState;
+
+            currentKeyboardState = Keyboard.GetState();
+            currentGamePadState = GamePad.GetState(PlayerIndex.One);
+            currentMouseState = Mouse.GetState();
+
+            mouse.Position.X = currentMouseState.X - mouse.BaseObjectTexture.Width / 2; mouse.Position.Y = currentMouseState.Y - mouse.BaseObjectTexture.Height / 2;
+        }
+
+        private void InitializeEnvironment()
+        {
+
+        }
+
+        private void InitializePlayer()
+        {
+
+        }
+
+        private void InitializeEnemies()
+        {
+
         }
     }
 }
